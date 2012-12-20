c1140e5317264cfa685bd334761fd86a99ad2de8
David Ladowitz <david@ladowitz.com>
2012-12-20 12:57:12 -0800

    copied from old GoDaddy hositng server

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Katrina::Application.config.secret_token = '1103cbcc1090990211faaeb6471fbcf9ba1fb01659549b3e9fc7ca71d7d156b399f6224ceacce3b7a11f2e77c2e25391bd95411b2d810787a1e7678b0c8ae654'
